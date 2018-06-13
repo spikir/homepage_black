f0fc1aa82b9a98a129b8a45b5c53ac24168e5002
spikir <spikirard@gmail.com>
2018-06-13 16:56:25 +0200

    Fadein content

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	var array = [];
-	
+	var slidearr = [];
+	var slidePos = "";
+	currentIndex = '';
+	var slidePos = '';
 	
 	$(".navi li a").click(function(){
 		$(".navi").find(".active").removeClass('active');
@@ -12,11 +15,34 @@ $(document).ready(function(){
 	
 	$(".navi li a").each(function(){
 		array.push($(this).attr("href"));
+		slidearr.push([$(this).attr("href"), false]);
 	});
+	
+	function findIndex(valueToSearch, array) {
+		if(Array.isArray(array[i])) {
+			for(var i = 0; i < array.length; i++) {
+				if(Array.isArray(array[i])) {
+					newIndex = findIndex(valueToSearch, array[i]);
+					if (newIndex) {
+						return newIndex;
+					} else if (array[i] == valueToSearch) {
+					   return currentIndex + i;
+				   }
+				} else if (array[i] == valueToSearch) {
+					return currentIndex + i;
+				}
+			}
+	}
 	
 	$(document).scroll(function(item) {
 		windowPos = $(window).scrollTop();
 		array.forEach(function(item) {
+			if(windowPos>=$(item).offset().top-700) {
+				$(item).fadeTo('slow', 1);
+				slidePos = findIndex(item, slidearr);
+				console.log(slidePos);
+				
+			}
 			if(windowPos>=$(item).offset().top-70) {
 				$('.navi').find('.active').removeClass('active');
 				$('.navi li a[href="'+item+'"]').parent().addClass("active");
